d280350fbad20b08b6da58f45c606667bd21d636
Aleksa <aleksa47@gmail.com>
2016-12-14 20:14:39 +0000

    Final version w/ missing field

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+    var ip = req.ip.split(':')[3]
+    var lang = req.get('Accept-Language')
+    var software = req.get('user-agent')
+    var retObj = {
+        ipaddress: ip,
+        language: lang,
+        // software: software
+    }
+    res.send(retObj)
+})
+
+app.listen(8080, function () {
+  console.log('Whoami app listening on port 8080!')
+})
